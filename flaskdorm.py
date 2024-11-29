fc449b558ea285b82223f4dcbae29873340f97b8
davidnuhwin <nguyendavid396@yahoo.com>
2024-11-28 20:14:46 -0800

    Created Dummy Code looped in index.html

--- flaskdorm.py
+++ flaskdorm.py
@@ -1,15 +1,34 @@
-from flask import Flask
+#Source: https://flask.palletsprojects.com/en/stable/quickstart/
+from flask import Flask, render_template
 app = Flask(__name__)
+
+
+dorm_suite = [
+    {
+        "area": "kitchen",
+        "description": "We be cooking.",
+        "objects": "Peter's T2GO Boxes and Kai's Socks"
+    },
+    {
+        "area": "living room",
+        "description": "Where the squad gathers for deep talks and Netflix binges.",
+        "objects": "Ryan's broken controller and Adriel's 'decorative' pile of laundry"
+    },
+
+]
+
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('index.html', dorm_suite=dorm_suite)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html')
 
 
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
